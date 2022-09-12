3428f55e245f70250f1c8a7855a6dd43e24336a3
Waelsi <waelsife@gmail.com>
2022-09-12 14:04:12 +0300

    second edit

--- TestPerson.java
+++ TestPerson.java
@@ -2,5 +2,6 @@ public class TestPerson {
     public static void main(String[] args) {
         Person obj1=new Person("Wael saif",22,168);
         System.out.println(obj1);
+        System.out.println("wael");
     }
 }
